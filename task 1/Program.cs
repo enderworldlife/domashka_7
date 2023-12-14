bc1db74aac76e8e43bfc9b993f6bbfdc34d3c592
Your Name <you@example.com>
2023-12-14 23:21:42 +0300

    zadanye 1

---
+++ task 1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// //zadanye 1
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// void PrintNumbers(int a, int b){
+//     if (a <= b){
+//         Console.Write(a + " ");
+//         PrintNumbers(a + 1, b);
+//     }
+// }
+// PrintNumbers(a, b);
+
+
+
+
