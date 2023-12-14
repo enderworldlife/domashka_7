069e4cef40a444668ac5d18c608d9ddbefa5761b
Your Name <you@example.com>
2023-12-14 23:56:52 +0300

    zadanye 3

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -15,26 +15,45 @@
 
 
 
-static int AckermannFunction (int m, int n){
-    if (m == 0){
-        return n + 1;
-    }
-    else if (n == 0){
-        return AckermannFunction(m - 1, 1);
-    }
-    else{
-        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+// static int AckermannFunction (int m, int n){
+//     if (m == 0){
+//         return n + 1;
+//     }
+//     else if (n == 0){
+//         return AckermannFunction(m - 1, 1);
+//     }
+//     else{
+//         return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+//     }
+// }
+
+
+
+// Console.Write("Введите значение m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int result = AckermannFunction(m, n);
+
+// Console.WriteLine($"Значение функции Аккермана для m {m} и n {n} равно {result}");
+
+
+
+
+static void PrintArrayReversed(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(array[index] + " ");
+        PrintArrayReversed(array, index - 1);
     }
 }
 
+int[] array = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Элементы массива, начиная с конца:");
+PrintArrayReversed(array, array.Length - 1);
 
 
-Console.Write("Введите значение m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите значение n: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-int result = AckermannFunction(m, n);
-
-Console.WriteLine($"Значение функции Аккермана для m {m} и n {n} равно {result}");
