61723413fa2501030452ce8f5c28e154fce5d384
Your Name <you@example.com>
2023-12-14 23:34:23 +0300

    zadanye 2

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -12,6 +12,34 @@
 // }
 // PrintNumbers(a, b);
 
+// zadanye 2
 
 
+
+
+static int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return AckermannFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
+
+Console.Write("Введите значение m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение n: ");
+int n = int.Parse(Console.ReadLine());
+
+int result = AckermannFunction(m, n);
+
+Console.WriteLine($"Значение функции Аккермана для m {m} и n {n} равно {result}");
 
